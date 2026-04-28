6fa64e1b830c9f35e5a682607b7ee2a171e63ed2
SM-Hussain08 <smuhammadhussain06@gmail.com>
2026-04-28 15:58:53 +0500

    Initial Humsafar React Firebase setup

---
+++ src/firebase/config.js
@@ -0,0 +1,25 @@
+// src/firebase/config.js
+
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+// Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCfPbYdg6zGPiIe0K7MLkK2bj_NYmABah0",
+  authDomain: "humsafar-app-3000b.firebaseapp.com",
+  projectId: "humsafar-app-3000b",
+  storageBucket: "humsafar-app-3000b.firebasestorage.app",
+  messagingSenderId: "494647434719",
+  appId: "1:494647434719:web:98675f73bc26a6af33386a",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Firebase services
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+
+// Export app (optional but recommended)
+export default app;
